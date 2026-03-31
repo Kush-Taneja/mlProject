d6245c0a76da6d4f7af147454c6c2a2ea10d32d0
Kush-Taneja <kushtaneja2712@gmail.com>
2026-03-31 13:17:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_req(file_path:str)->List[str]:
+    '''
+    this will return list of req
+    '''
+    req = []
+    with open(file_path) as file_obj:
+        req = file_obj.readlines()
+        req = [r.replace("\n","") for r in req]
+
+        if HYPEN_E_DOT in req:
+            req.remove(HYPEN_E_DOT)
+    
+    return req
+
+setup(
+    name='mlProject',
+    version='0.0.1',
+    author='Kush',
+    author_email='kushtaneja2712@gmail.com',
+    packages=find_packages(),
+    install_requires=get_req('requirements.txt')
+)
